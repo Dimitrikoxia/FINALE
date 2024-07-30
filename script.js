60e745fd84dfb3d477b70d1e24eb23735ed45256
Dimitrikoxia <dimitrikoxia@gmail.com>
2024-07-30 14:46:42 +0400

    FINALE

---
+++ script.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector(".burger")
+let xxmark = document.querySelector(".xmark")
+let header_nav = document.querySelector("header_nav")
+
+burger.addEventListener("click", ()=>{
+     header_nav1.classlist.add("burger")
+})
